b3f44139ad6402777cdee670556501e5ebd6c8c4
Joel Hobgood <jhobgoo@gmail.com>
2020-03-23 03:15:13 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -10,4 +10,10 @@ class MusicLibraryController
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
   
+  def import
+    files.each do |file| 
+      Song.create_from_filename(file)
+    end
+  end
+  
 end
