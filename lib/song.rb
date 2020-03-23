99c854501e99d1944c9fd22d1740e23b8da553f3
Joel Hobgood <jhobgoo@gmail.com>
2020-03-23 03:09:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -64,7 +64,7 @@ class Song
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_category)
 
-    new(song_name, artist, genre)
+    new(song_title, artist, genre)
   end
   
 end
