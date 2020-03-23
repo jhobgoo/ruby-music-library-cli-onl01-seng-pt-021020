9460eaf24b9633c2484152c7dfe665e5e9550d4b
Joel Hobgood <jhobgoo@gmail.com>
2020-03-23 03:05:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,8 +59,8 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    file_name = name.split(" - ")
-    artist_name, song_title, genre_category = file_name[0], file_name[1], file_name[2].gsub(".mp3", "")
+    file = name.split(" - ")
+    artist_name, song_title, genre_category = file[0], file[1], file[2].gsub(".mp3", "")
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_category)
 
