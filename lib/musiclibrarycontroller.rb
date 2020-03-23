da70cf928d3fa6583c4d6bed5320b0af34a449c6
Joel Hobgood <jhobgoo@gmail.com>
2020-03-23 03:15:46 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -1,19 +1,9 @@
 class MusicLibraryController
 
-  attr_reader :path  
+  attr_accessor :path  
 
   def initialize(path)
     @path = path
-  end
-  
-  def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-  end
-  
-  def import
-    files.each do |file| 
-      Song.create_from_filename(file)
-    end
   end
   
 end
