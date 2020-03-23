c5e4938d90f7154f6390044edcee5db1a62c7128
Joel Hobgood <jhobgoo@gmail.com>
2020-03-23 02:41:43 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,9 @@
 module Concerns::Findable
   
-  module ClassMethods
-     
-    def find_by_name(name)
-      @@all.detect do |song|
-        song.name == name
-      end
+  def find_by_name(name)
+    @@all.detect do |song|
+      song.name == name
     end
-    
   end
-  
+    
 end
